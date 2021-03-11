c6148874f1ee403f2bae5cdb29b8536486a2a63f
Sávio Da Costa Santos <saviocosta.flam20@gmail.com>
2021-03-11 13:18:23 -0300

    Activity Nova

---
+++ app/src/main/java/com/e/testeagenda/FuncionalidadeNovaActivity.kt
@@ -0,0 +1,10 @@
+package com.e.testeagenda
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FuncionalidadeNovaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
